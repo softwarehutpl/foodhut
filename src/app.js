6653ef4d8a943312485f4f351f28b5314fd0631c
jurszewicz <filip.jurszewicz@gmail.com>
2016-08-01 14:14:57 +0200

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
+import './styles/style.css';
+
 import Hello from './content/hello.jsx';
 import World from './content/world.jsx';
-require('./styles/style.css');
-document.write(require('./content/content.js'));
