401b7fd0f0fa17e7ffb942c690756826fea144bb
jurszewicz <filip.jurszewicz@gmail.com>
2016-08-01 13:37:34 +0200

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
-        ]
-    }
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.js$/, loader: 'babel', exclude: /(node_modules)/, query: { presets: ['es2015'] } },
+        ],
+    },
 };
