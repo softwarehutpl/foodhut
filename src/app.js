418eb393605c5c5f49cffd710c938c17c239051e
jurszewicz <filip.jurszewicz@gmail.com>
2016-08-03 10:30:40 +0200

    clean up

--- src/app.js
+++ src/app.js
@@ -6,30 +6,7 @@ import './styles/style.css';
 import Hello from './content/hello.jsx';
 import World from './content/world.jsx';
 
-import $order from './services/order.service.js';
-import $test from './services/test.service.js';
 
-var orderService = new $order();
-var testService = new $test();
+import Test from './views/test/test.module.jsx';
 
-testService.getTestRestaurant()
-	.then(restaurant => {
-		return orderService.addOrder(restaurant.id);
-	})
-	.then(()=>{
-		console.log('added new order');
-		return;
-	})
-	.then(()=> {
-		return orderService.getOrders();
-	})
-	.then(orders => {
-		console.log('all orders', orders);
-	})
-	.then(()=> {
-		console.log('end of test');
-	})
-
-// import Test from './views/test/test.module.jsx';
-
-// ReactDOM.render(Test, document.querySelector('div'));
+ReactDOM.render(Test, document.querySelector('div'));
